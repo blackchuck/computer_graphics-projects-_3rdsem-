d807b82434b402bbdaf314410925662336aaae69
Roshan Kunwar <133420437+blackchuck@users.noreply.github.com>
2024-03-06 17:39:56 +0545

    Update cg pro.cpp

--- cg pro.cpp
+++ cg pro.cpp
@@ -13,24 +13,7 @@
 	PROJECT:COMPUTER GRAPHICS PROJECT FOR FOURTH SEMEMSTER(BATCH-2078)
         LAB INSTRUCTOR:Er.RAMESH SHARMA	
     
-    colour code numerical value:
-    0.black
-    1.blue
-    2.green
-    3.cyan
-    4.red
-    5.magneta
-    6.brown
-    7.light gray
-    8.dark gray
-    9.light blue
-    10.light green
-    11.light cyan
-    12.light red
-    13.light magneta
-    14.yellow
-    15.white
-    
+
 	
 */
 void handDown(int i)
